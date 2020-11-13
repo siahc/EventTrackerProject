6306952f12d8dbf23b797275bd1d7c99d421f525
Josiah Candler <josiahcandler@Josiahs-MacBook-Pro.local>
2020-11-13 16:41:53 -0700

    Stub Boot project with JPA project dependency

---
+++ PlantSale/src/main/java/com/skilldistillery/plantsale/PlantSaleApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.plantsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantSaleApplication.class, args);
+	}
+
+}
---
+++ PlantSale/src/test/java/com/skilldistillery/plantsale/PlantSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.plantsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
