89d7615a7be7375ee3fa5005853463fc25fb3055
Josiah Candler <69128122+siahc@users.noreply.github.com>
2020-11-22 15:44:15 -0700

    Stub out Plant Detail Plant Update and Delete

--- PlantSale/src/main/resources/static/js/eventTracker.js
+++ PlantSale/src/main/resources/static/js/eventTracker.js
@@ -7,7 +7,8 @@ window.addEventListener('load', function(){
 function init() {
 	console.log('in init() still ');
 		//TODO: set up event listeners for buttons etc.
-	toggleCreateForm();
+	hide('updateFormDiv')
+	hide('createFormDiv')
 	listPlants();
 }
 
@@ -48,6 +49,10 @@ function displayPlantList(list){
 	for (const plant of list) {
 		let tr = document.createElement('tr');
 
+		tr.onclick = function(e) {
+			showUpdateForm(plant.id);
+		};
+
 		// loop through keys
 		for (const key of keys) {
 			// make a td
@@ -66,6 +71,48 @@ function displayPlantList(list){
 	plTable.replaceWith(table )
 }
 
+function showUpdateForm(id) {
+	console.log(id)
+
+	// make an xhr object
+	// GET /api/plants/ + id
+	// only on success
+		// parse plant		
+		// set values for all fields in the update form
+		// getElementById('update_id').value = plant.id
+		// getElementById('update_name').value = plant.name
+		// getElementById('update_rare').checked = plant.rare
+		// ... etc for all fields
+		window.location = '#' + id // TODO: back button still not working in update form
+		hide('plantList')
+		show('updateFormDiv')
+}
+
+function updatePlant() {
+	// get the update_id
+	// configure PATCH to the /api/plants/ + id
+	// parse the form, build a plant obj
+		// see createPlant() for form parsing logic
+	// validate form
+	// call PATCH to the server
+	// on success print and then show table
+		window.location = ''
+		hide('updateFormDiv')
+		listPlants()
+	// otherwise
+		// print the error to the update_status element
+}
+
+function deletePlant() {
+	// get the update_id
+	// call DELETE to the server
+	// on success print and then show table
+		window.location = ''
+		hide('updateFormDiv')
+		listPlants()
+	// otherwise
+		// print the error to the delete_status element
+}
 
 function displayInventoryValue(list) {
 	let total = 0;
@@ -76,8 +123,8 @@ function displayInventoryValue(list) {
 }
 
 // https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
-function toggleCreateForm() {
-	let form = document.getElementById('createFormDiv');
+function toggleHide(elementId) {
+	let form = document.getElementById(elementId);
 	if (form.style.display === 'none') {
 		form.style.display = 'block';
 	} else {
@@ -85,23 +132,38 @@ function toggleCreateForm() {
 	}
 }
 
+function hide(elementId) {
+	let form = document.getElementById(elementId);
+	form.style.display = 'none';
+}
+
+function show(elementId) {
+	let form = document.getElementById(elementId);
+	form.style.display = 'block';
+}
+
 function createPlant(){
-	let price = parseFloat(document.getElementById('create_price').value)
-	price = Math.round(price * 100) /100
 	let plant = {
 		"name": document.getElementById('create_name').value,
 		"description": document.getElementById('create_description').value,
-		"price": price,
 		"rare": document.getElementById('create_rare').checked,
 		"variegation": document.getElementById('create_variegation').checked,
 		"image": document.getElementById('create_image').value,
 	}
+
+	let priceStr = document.getElementById('create_price').value
+	if (priceStr !== "") {
+		let price = parseFloat(priceStr)
+		price = Math.round(price * 100) /100	
+		plant.price = price
+	}
+
 	console.log(plant)
 	console.log(JSON.stringify(plant))
 
 	let status = document.getElementById('create_status')
 	// Validate the form client side
-	if (Number.isNaN(plant.price)) {
+	if (plant.price !== null && Number.isNaN(plant.price)) {
 		status.textContent = "form error:  Price is not a number"
 		return
 	}
