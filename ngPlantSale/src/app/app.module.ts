6bd4aad33e300625c89c8e72a64deb7dd26f3fe2
Josiah Candler <69128122+siahc@users.noreply.github.com>
2020-11-27 17:46:56 -0700

    Generate plant model service and list component

--- ngPlantSale/src/app/app.module.ts
+++ ngPlantSale/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlantListComponent } from './components/plant-list/plant-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlantListComponent
   ],
   imports: [
     BrowserModule,
